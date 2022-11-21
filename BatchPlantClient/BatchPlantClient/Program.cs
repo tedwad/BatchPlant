52af9dc968d5aa442ee3a6093101e8045a9beb7a
tedwad <89061708+tedwad@users.noreply.github.com>
2022-11-21 10:42:45 -0500

    BatchPlantClient commit

---
+++ BatchPlantClient/BatchPlantClient/Program.cs
@@ -0,0 +1,73 @@
+﻿using Opc.Ua;
+using Opc.Ua.Client;
+using Opc.Ua.Configuration;
+using System;
+
+namespace BatchPlantClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Configuring UA Client");
+            var config = new ApplicationConfiguration()
+            {
+                ApplicationName = "BatchPlantClient",
+                ApplicationUri = Utils.Format(@"urn:{0}:BatchPlantClient", System.Net.Dns.GetHostName()),
+                ApplicationType = ApplicationType.Client,
+                SecurityConfiguration = new SecurityConfiguration
+                {
+                    ApplicationCertificate = new CertificateIdentifier { StoreType = @"Directory", StorePath = @"%CommonApplicationData%\OPC Foundation\CertificateStores\MachineDefault", SubjectName = "BatchPlantClient" },
+                    TrustedIssuerCertificates = new CertificateTrustList { StoreType = @"Directory", StorePath = @"%CommonApplicationData%\OPC Foundation\CertificateStores\UA Certificate Authorities" },
+                    TrustedPeerCertificates = new CertificateTrustList { StoreType = @"Directory", StorePath = @"%CommonApplicationData%\OPC Foundation\CertificateStores\UA Applications" },
+                    RejectedCertificateStore = new CertificateTrustList { StoreType = @"Directory", StorePath = @"%CommonApplicationData%\OPC Foundation\CertificateStores\RejectedCertificates" },
+                    AutoAcceptUntrustedCertificates = true
+                },
+                TransportConfigurations = new TransportConfigurationCollection(),
+                TransportQuotas = new TransportQuotas { OperationTimeout = 15000 },
+                ClientConfiguration = new ClientConfiguration { DefaultSessionTimeout = 60000 },
+                TraceConfiguration = new TraceConfiguration()
+            };
+            config.Validate(ApplicationType.Client).GetAwaiter().GetResult();
+            if (config.SecurityConfiguration.AutoAcceptUntrustedCertificates)
+            {
+                config.CertificateValidator.CertificateValidation += (s, e) => { e.Accept = (e.Error.StatusCode == StatusCodes.BadCertificateUntrusted); };
+            }
+
+
+            var application = new ApplicationInstance
+            {
+                ApplicationName = "BatchPlantClient",
+                ApplicationType = ApplicationType.Client,
+                ApplicationConfiguration = config
+            };
+
+            application.CheckApplicationInstanceCertificate(false, 2048).GetAwaiter().GetResult();
+
+            var selectedEndpoint = CoreClientUtils.SelectEndpoint("opc.tcp://desktop-keg18p4:26543/BatchPlantServer", useSecurity: true);
+
+
+            using (var session = Session.Create(config, new ConfiguredEndpoint(null, selectedEndpoint, EndpointConfiguration.Create(config)), false, "", 60000, null, null).GetAwaiter().GetResult())
+            {
+                Console.WriteLine("Step 3 - Browse the server namespace.");
+                ReferenceDescriptionCollection refs;
+                Byte[] cp;
+                session.Browse(null, null, ObjectIds.ObjectsFolder, 0u, BrowseDirection.Forward, ReferenceTypeIds.HierarchicalReferences, true, (uint)NodeClass.Variable | (uint)NodeClass.Object | (uint)NodeClass.Method, out cp, out refs);
+                Console.WriteLine("DisplayName: BrowseName, NodeClass");
+                foreach (var rd in refs)
+                {
+                    Console.WriteLine("{0}: {1}, {2}", rd.DisplayName, rd.BrowseName, rd.NodeClass, rd.NodeId);
+                    ReferenceDescriptionCollection nextRefs;
+                    byte[] nextCp;
+                    session.Browse(null, null, ExpandedNodeId.ToNodeId(rd.NodeId, session.NamespaceUris), 0u, BrowseDirection.Forward, ReferenceTypeIds.HierarchicalReferences, true, (uint)NodeClass.Variable | (uint)NodeClass.Object | (uint)NodeClass.Method, out nextCp, out nextRefs);
+                    foreach (var nextRd in nextRefs)
+                    {
+                        Console.WriteLine("+ {0}: {1}, {2}", nextRd.DisplayName, nextRd.BrowseName, nextRd.NodeClass, nextRd.NodeId);
+                    }
+                }
+            }
+
+
+        }
+    }
+}
